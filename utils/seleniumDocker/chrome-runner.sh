c6ec8c18eed6ee0fa1c151e05fd06ce478d955c5
artemm <artemm@seedbox.artemm.info>
2023-12-29 21:46:51 +0200

    Updated selenium container; adjusted chrome run options to start extension out of the box

--- utils/seleniumDocker/chrome-runner.sh
+++ utils/seleniumDocker/chrome-runner.sh
@@ -1,7 +1,8 @@
-rm -rf /home/seluser/.config/google-chrome/Singleton* 
+rm -rf ~/.config/google-chrome/Singleton* 
+xdg-settings set default-web-browser google-chrome
 sleep 10;
 #Just opening random url to "warm up". All this ugly hack is needed because if I open chrome with multiple urls they are not always loaded, need to do things like this to avoid
-google-chrome --disable-accelerated-2d-canvas --disable-gpu --hide-crash-restore-bubble  --no-sandbox \
+google-chrome --load-extension=~/PTRentNotifier/ --disable-fre --no-default-browser-check --no-first-run --disable-accelerated-2d-canvas --disable-gpu --hide-crash-restore-bubble  --no-sandbox \
 	"http://google.com" &
 sleep 30;
 
@@ -9,7 +10,7 @@ urls=$(echo $TARGET_URLS | tr " ||; " "\n")
 
 for url in $urls
 do
-	google-chrome --disable-accelerated-2d-canvas --disable-gpu --hide-crash-restore-bubble  --no-sandbox \
+	google-chrome --load-extension=~/PTRentNotifier/ --disable-accelerated-2d-canvas --disable-gpu --hide-crash-restore-bubble  --no-sandbox \
 	$url & sleep 30;
 	echo "Opening URL: $url"
 done
