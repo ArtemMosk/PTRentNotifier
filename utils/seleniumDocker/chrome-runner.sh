cc5d1d6d5fd9a6183b1c5773de7fb77a9cbd4ad4
artemm <artemm@seedbox.artemm.info>
2023-12-30 14:04:17 +0200

    Removed google tab opening at the beginning

--- utils/seleniumDocker/chrome-runner.sh
+++ utils/seleniumDocker/chrome-runner.sh
@@ -1,10 +1,5 @@
 rm -rf ~/.config/google-chrome/Singleton* 
 xdg-settings set default-web-browser google-chrome
-sleep 10;
-#Just opening random url to "warm up". All this ugly hack is needed because if I open chrome with multiple urls they are not always loaded, need to do things like this to avoid
-google-chrome --load-extension=~/PTRentNotifier/ --disable-fre --no-default-browser-check --no-first-run --disable-accelerated-2d-canvas --disable-gpu --hide-crash-restore-bubble  --no-sandbox \
-	"http://google.com" &
-sleep 30;
 
 urls=$(echo $TARGET_URLS | tr " ||; " "\n")
 
