40ce712cbeac3e58fef04e04586a4f725496cdc6
artemm <artemm@seedbox.artemm.info>
2024-01-13 23:51:59 +0200

    Added adjustable open timeout for chrome running; Fixed typo in password env variable

--- utils/seleniumDocker/chrome-runner.sh
+++ utils/seleniumDocker/chrome-runner.sh
@@ -1,11 +1,24 @@
-rm -rf ~/.config/google-chrome/Singleton* 
+#!/bin/sh
+
+URL_OPEN_TIMEOUT="${URL_OPEN_TIMEOUT:-30}"  # Set default to 30 if not set
+FIRST_SLEEP_DURATION=$(($URL_OPEN_TIMEOUT / 3))
+
+# Initialize an empty string for the proxy argument
+PROXY_ARG=""
+
+# Check if the CHROME_PROXY environment variable is set
+if [ -n "$CHROME_PROXY" ]; then
+	    # Form the proxy argument
+	PROXY_ARG="--proxy-server=$CHROME_PROXY"
+fi
+
+rm -rf ~/.config/google-chrome/Singleton*
 xdg-settings set default-web-browser google-chrome
 
 urls=$(echo $TARGET_URLS | tr " ||; " "\n")
-
+sleep $FIRST_SLEEP_DURATION
 for url in $urls
 do
-	google-chrome --load-extension=~/PTRentNotifier/ --disable-accelerated-2d-canvas --disable-gpu --hide-crash-restore-bubble  --no-sandbox \
-	$url & sleep 30;
+	google-chrome $PROXY_ARG --load-extension=~/PTRentNotifier/ --disable-accelerated-2d-canvas --disable-gpu --hide-crash-restore-bubble --no-sandbox "$url" & sleep $URL_OPEN_TIMEOUT;
 	echo "Opening URL: $url"
 done
